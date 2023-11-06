9bae2ba20d8fa1e190cb8bd882155aad6343825e
Hafiz Taha Ashraf <135725148+tahashraf2001@users.noreply.github.com>
2023-11-06 23:09:35 +0500

    Add files via upload
    Calculator

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const taskInput = document.getElementById('task');
-    const addTaskButton = document.getElementById('addTask');
-    const taskList = document.getElementById('taskList');
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
 
-    addTaskButton.addEventListener('click', function () {
-        const taskText = taskInput.value.trim();
-        if (taskText !== '') {
-            const li = document.createElement('li');
-            li.innerHTML = `
-                <span>${taskText}</span>
-                <button class="delete-button">Delete</button>
-            `;
-            taskList.appendChild(li);
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
 
-            // Clear input field
-            taskInput.value = '';
-
-            // Attach delete event listener to new task
-            const deleteButton = li.querySelector('.delete-button');
-            deleteButton.addEventListener('click', function () {
-                taskList.removeChild(li);
-            });
-        }
-    });
-
-    // Enter key to add a task
-    taskInput.addEventListener('keyup', function (event) {
-        if (event.key === 'Enter') {
-            addTaskButton.click();
-        }
-    });
-});
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (e) {
+        document.getElementById('display').value = 'Error';
+    }
+}
